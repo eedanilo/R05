74e0de2ee532a51d44d517470684ecd9df6b87e8
eedanilo <ewerthon.tavares@ccc.ufcg.edu.br>
2025-12-03 10:00:45 -0300

    Floor.

--- src/main/java/problems/FloorCeilBinarySearchImpl.java
+++ src/main/java/problems/FloorCeilBinarySearchImpl.java
@@ -15,24 +15,35 @@ public class FloorCeilBinarySearchImpl implements FloorCeil {
 
 	@Override
 	public Integer floor(Integer[] array, Integer x) {
-		int l = 0;
-		int r = array.length - 1;
-		public Integer bsFloor(Integer[] array, int x, int l, int r) {
-			int m = (l + r) / 2;
-			if (array[m] <= x && array[m + 1] > x) {
-				return array[m];
-			}
-			if (array[m] > x) {
-				return bsFloor(array, x, l, array[m - 1]);
-			}
-			if (array[m] < x) {
-				return bsFloor(array, x, array[m + 1], r);
-			}
-		}}
+		return bsFloor(array, x, 0, array.length - 1);
+	}
+	public Integer bsFloor(Integer[] array, int x, int l, int r) {
+		if (l > r){
+			return null;
+		}
+		int m = (l + r) / 2;
+		if (array[m] <= x && array[m + 1] > x) {
+			return array[m];
+		}
+		if (array[m] > x) {
+			return bsFloor(array, x, l, m - 1);
+		}
+		if (array[m] < x) {
+			return bsFloor(array, x, m + 1, r);
+		}
+	}
 
 	@Override
 	public Integer ceil(Integer[] array, Integer x) {
-		
+		return bsCeil(array, x, 0, array.length - 1);
+	}
+
+	public bsCeil(Integer[] array, int x, int l, int r) {
+		int m = (l + r) / 2;
+		if (array[m] >= x && array[m - 1] < x) {
+			return array[m];
+		}
+		if (array[m] > x && array[m - 1] > x)
 	}
 
 }
